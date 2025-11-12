8a52a0525a52cc35db485ea921f2d126f6f48e85
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-12 12:18:25 +0700

    menambahkan endpoint POST

--- index.js
+++ index.js
@@ -25,6 +25,27 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.post('/create', (req, res) => {
+  const apiKey = crypto.randomBytes(16).toString('hex');
+
+  db.query('INSERT INTO api_keys (api_key) VALUES (?)', [apiKey], (err) => {
+    if (err) {
+      console.error('Gagal menyimpan API key:', err);
+      return res.status(500).json({ 
+        success: false,
+        message: 'Terjadi kesalahan saat menyimpan API Key!'
+      });
+    }
+    res.json({ 
+      success: true,
+      message: 'API Key berhasil dibuat dan disimpan!',
+      apiKey 
+    });
+  });
+});
+
+
+
 
 
 
