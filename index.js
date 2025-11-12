4a3462e88b6991db84441d7b873dc5325a34f010
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-12 12:15:39 +0700

    menambahkan koneksi ke database MySQL

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  port: 3309,  
+  user: 'root',      
+  password: 'Aiszr131004',    
+  database: 'apikey_db'
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('Terhubung ke database MySQL');
+});
+
+
